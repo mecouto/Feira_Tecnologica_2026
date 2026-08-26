0ad83158113e6bc6fa5b1112e17a43d34690877b
JoaoGabini <joaogabini03@gmail.com>
2026-08-26 17:13:46 -0300

    tentando finalizar js

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const pool = require('./config/database');
+
 const app = express();
 
 app.use(cors());
@@ -18,3 +20,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
+
